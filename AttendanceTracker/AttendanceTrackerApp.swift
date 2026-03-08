7d1ae643a734bd272cc6fd5d3d1a52807df27055
Pranav Krishna <pranavkrishnap856@gmail.com>
2026-03-08 16:40:02 +0530

    Initial Commit

---
+++ AttendanceTracker/AttendanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttendanceTrackerApp.swift
+//  AttendanceTracker
+//
+//  Created by Pranav Krishna P on 08/03/26.
+//
+
+import SwiftUI
+
+@main
+struct AttendanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AttendanceTrackerTests/AttendanceTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AttendanceTrackerTests.swift
+//  AttendanceTrackerTests
+//
+//  Created by Pranav Krishna P on 08/03/26.
+//
+
+import Testing
+@testable import AttendanceTracker
+
+struct AttendanceTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
